99e0b1032742fed095b86654bff459629203d659
hairenjun <hairenjun@hotmail.com>
2024-06-13 18:12:56 -0700

    增加GET请求适配

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
+const { URL } = require('url');
 
 const app = express();
 const PORT = 3000;
-
 app.use(bodyParser.json());
 
 // POST请求路由
@@ -22,12 +22,21 @@ app.post('/', async (req, res) => {
 app.get('/', async (req, res) => {
     const { endpoint } = req.query;
     try {
-        const response = await axios.get(endpoint);
-        res.json(response.data);
+        const response = await axios.get(endpoint, { responseType: 'stream' });
+
+        // 获取目标网站返回的 Content-Type
+        const contentType = response.headers['content-type'];
+        if (contentType) {
+            res.set('Content-Type', contentType);
+        }
+
+        response.data.pipe(res); // 将目标网站的响应直接传递给客户端
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 });
+
+
 
 // 启动服务器
 app.listen(PORT, () => {
